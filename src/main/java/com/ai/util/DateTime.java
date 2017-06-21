dd302c8cee2b16c5324c6c557187f995a359aea0
OTTER <893812347@qq.com>
2017-06-21 22:49:01 +0800

    添加today，返回yyyyMMdd格式字符串

--- src/main/java/com/ai/util/DateTime.java
+++ src/main/java/com/ai/util/DateTime.java
@@ -6,18 +6,26 @@ import java.util.Date;
 
 public class DateTime {
 	public static final String datePatter="yyyy-MM-dd HH:mm:ss SSS";
+	
 	public static String now(){
 		   return formatDate(new Date());
 	   }
-	   public static String formatDate(Date date){
-			SimpleDateFormat simpleDateFormat=new SimpleDateFormat(datePatter);
-			String formatedDate=simpleDateFormat.format(date);
-			return formatedDate;
-	   }
+	
+	public static String today(){
+		SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyyMMdd");
+		String formatedDate=simpleDateFormat.format(new Date());
+		return formatedDate;
+	}
+	
+	public static String formatDate(Date date){
+		SimpleDateFormat simpleDateFormat=new SimpleDateFormat(datePatter);
+		String formatedDate=simpleDateFormat.format(date);
+		return formatedDate;
+	}
 
-	   public static Date parseDate(String formetedDate) throws ParseException{
+	public static Date parseDate(String formetedDate) throws ParseException{
 		SimpleDateFormat simpleDateFormat=new SimpleDateFormat(datePatter);
 		Date formatedDate=simpleDateFormat.parse(formetedDate);
 		return formatedDate;
-	   }
+	}
 }
