7290ceed51085241d2351e77504fa932876a4b24
OTTER <893812347@qq.com>
2017-07-01 15:31:47 +0800

    調整代碼解構

--- src/main/java/com/ai/App.java
+++ src/main/java/com/ai/App.java
@@ -1,9 +1,29 @@
 package com.ai;
+
 import com.ai.xml.XMLDocument;
+import com.ai.util.Log;
+import com.ai.crawl.FetchManager;;
 
-public class App {
-
+public class App {	
+	private static Log log =Log.getLog(App.class.getName());
+	
 	public static void main(String[] args) {
+		testXML();
+		try {			
+			log.debug("main begin");
+			String localSaveDir="";
+			if(args.length==1)
+				localSaveDir=args[0];
+			if(!localSaveDir.equals(""))
+			  crawl(localSaveDir);
+			log.debug("main end");
+		} catch (Exception e) {
+			log.error(e);
+			e.printStackTrace();
+		}	
+	}
+	
+	public static void testXML(){
 		try {
 			XMLDocument document=new XMLDocument();
 			document.loadFromFile("D:\\Book.xml");
@@ -14,8 +34,17 @@ public class App {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}	
 		
-		
-	}
+	public static void crawl(String localSaveDirectory){
+			log.info("LocalSaveDirectory:" + localSaveDirectory);
+			if (localSaveDirectory.equals("")) {
+				log.error("Illegal input parameter");
+				return;
+			}
+			FetchManager fetchManager = FetchManager.getInstance();
+			fetchManager.setLocalSaveDirectory(localSaveDirectory);
+			fetchManager.run();
+		}
 
 }
