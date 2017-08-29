b681bf519fab8d31d2cc55d8cb38def5bb4cd500
OTTER <893812347@qq.com>
2017-08-29 23:00:30 +0800

    单一数据源时可去掉MapperScan注解的sqlSessionFactoryRef属性

--- src/main/java/com/ai/demoes/mybatis/DataSourceConfig.java
+++ src/main/java/com/ai/demoes/mybatis/DataSourceConfig.java
@@ -14,69 +14,67 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-
 import com.alibaba.druid.pool.DruidDataSource;
 
 @Configuration
-@MapperScan(basePackages = "com.ai.demoes.mybatis.mapper", sqlSessionFactoryRef = "webPageSqlSessionFactory")
+@MapperScan(basePackages = "com.ai.demoes.mybatis.mapper")
 public class DataSourceConfig {
 	@Value("${Druid.jdbcDriver}")
 	String jdbcDriverName;
-	
+
 	@Value("${Druid.dbURL}")
 	String dburl;
-	
+
 	@Value("${Druid.DBUserName}")
 	String userName;
-	
+
 	@Value("${Druid.DBPassword}")
 	String password;
-	
+
 	@Value("${Druid.Filter}")
 	String filter;
-	
+
 	@Value("${Druid.MaxActive}")
 	int maxActive;
-	
+
 	@Value("${Druid.MaxWait}")
 	int maxWait;
-	
+
 	@Value("${Druid.MinIdle}")
 	int minIdle;
-	
+
 	@Value("${Druid.TimeBetweenEvictionRunsMillis}")
 	int timeBetweenEvictionRunsMillis;
-	
+
 	@Value("${Druid.MinEvictableIdleTimeMillis}")
 	int minEvictableIdleTimeMillis;
-	
+
 	@Value("${Druid.TestWhileIdle}")
 	boolean testWhileIdle;
-	
+
 	@Value("${Druid.TestOnBorrow}")
 	boolean testOnBorrow;
-	
+
 	@Value("${Druid.TestOnReturn}")
 	boolean testOnReturn;
-	
+
 	@Value("${Druid.PoolPreparedStatements}")
 	boolean poolPreparedStatements;
-	
+
 	@Value("${Druid.MaxOpenPreparedStatements}")
 	int maxOpenPreparedStatements;
-	
+
 	@Value("${Druid.ValidationQuery}")
 	String validationQuery;
 	private static final String MAPPER_PATH = "classpath*:/Mapper/*.xml";
 
-    private static final String ENTITY_PACKAGE = "com.ai.demoes.mybatis.entity";
+	private static final String ENTITY_PACKAGE = "com.ai.demoes.mybatis.entity";
 
-    @Bean(name = "webPageDataSource")
-    @Primary
-    public DataSource webPageDataSource() throws SQLException {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName(jdbcDriverName);
+	@Bean(name = "webPageDataSource")
+	@Primary
+	public DataSource webPageDataSource() throws SQLException {
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setDriverClassName(jdbcDriverName);
 		dataSource.setUrl(dburl);
 		dataSource.setUsername(userName);
 		dataSource.setPassword(password);
@@ -95,26 +93,18 @@ public class DataSourceConfig {
 		dataSource.setRemoveAbandoned(false);
 		dataSource.setRemoveAbandonedTimeout(180);
 		dataSource.init();
-        return dataSource;
-    }
+		return dataSource;
+	}
 
-    @Bean(name = "webPageTransactionManager")
-    @Primary
-    public DataSourceTransactionManager webPageTransactionManager()
-            throws SQLException {
-        return new DataSourceTransactionManager(webPageDataSource());
-    }
-
-    @Bean(name = "webPageSqlSessionFactory")
-    @Primary
-    public SqlSessionFactory risSqlSessionFactory(
-            @Qualifier("webPageDataSource") DataSource webPageDataSource)
-            throws Exception {
-        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(webPageDataSource);
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sessionFactory.setMapperLocations(resolver.getResources(MAPPER_PATH));
-        sessionFactory.setTypeAliasesPackage(ENTITY_PACKAGE);
-        return sessionFactory.getObject();
-    }
+	@Bean(name = "webPageSqlSessionFactory")
+	@Primary
+	public SqlSessionFactory webPageSqlSessionFactory(@Qualifier("webPageDataSource") DataSource webPageDataSource)
+			throws Exception {
+		final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(webPageDataSource);
+		ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		sessionFactory.setMapperLocations(resolver.getResources(MAPPER_PATH));
+		sessionFactory.setTypeAliasesPackage(ENTITY_PACKAGE);
+		return sessionFactory.getObject();
+	}
 }
