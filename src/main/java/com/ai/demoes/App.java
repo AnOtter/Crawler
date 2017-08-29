224d4e2f1f55bf1f4b78a07029078aa42206689e
OTTER <893812347@qq.com>
2017-08-29 20:58:29 +0800

    去掉主程序类中的MapperScan注解

--- src/main/java/com/ai/demoes/App.java
+++ src/main/java/com/ai/demoes/App.java
@@ -1,11 +1,9 @@
 package com.ai.demoes;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages="com.ai.demos")
 public class App {
 	public static void main(String[] args) {		
 		try {
