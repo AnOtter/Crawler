4776781f87b3e5463b7b13300b87ee114556e82b
OTTER <893812347@qq.com>
2017-09-14 21:56:34 +0800

    删除多余引用

--- src/main/java/com/ai/crawlerweb/App.java
+++ src/main/java/com/ai/crawlerweb/App.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
